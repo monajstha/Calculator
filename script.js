c104285fc9304d91e91555465c9af42b56df2cc3
monajstha <manojstha700@gmail.com>
2024-06-11 19:29:07 +0545

    add: add operate function

--- script.js
+++ script.js
@@ -18,6 +18,26 @@ const divide = function (num1, num2) {
   return num1 / num2;
 };
 
+const operate = function (num1, operator, num2) {
+  let result;
+  switch (operator) {
+    case "+":
+      result = add(num1, num2);
+      break;
+    case "-":
+      result = subtract(num1, num2);
+      break;
+    case "*":
+      result = multiply(num1, num2);
+      break;
+    case "/":
+      result = divide(num1, num2);
+      break;
+    default:
+      break;
+  }
+};
+
 module.exports = {
   add,
   subtract,
