22a43eaafc7adfd5738665ebfc9dcfeb9e42fe66
monajstha <manojstha700@gmail.com>
2024-06-11 19:28:39 +0545

    add basic mathematical functions

---
+++ script.js
@@ -0,0 +1,26 @@
+let firstNumber;
+let operator;
+let lastNumber;
+
+const add = function (num1, num2) {
+  return num1 + num2;
+};
+
+const subtract = function (num1, num2) {
+  return num1 - num2;
+};
+
+const multiply = function (num1, num2) {
+  return num1 * num2;
+};
+
+const divide = function (num1, num2) {
+  return num1 / num2;
+};
+
+module.exports = {
+  add,
+  subtract,
+  multiply,
+  divide,
+};
